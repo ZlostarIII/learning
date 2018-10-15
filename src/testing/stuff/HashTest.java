04ea24f5173892682f8f414ffa9c35b801e0ccee
Vladimir Saev <Vladimir_Saev@epam.com>
2018-10-15 16:40:24 +0300

    Third commit - hash code

--- src/testing/stuff/HashTest.java
+++ src/testing/stuff/HashTest.java
@@ -4,6 +4,11 @@ public class HashTest {
 
 	public static void main(String[] args) {
 		System.out.println(hash());
+		
+		System.out.println(areEqual(new HashClass("a"), new HashClass("a")));
+		System.out.println(new HashClass("a").hashCode());
+		System.out.println(new HashClass("a").hashCode());
+		System.out.println(new HashClass("b").hashCode());
 	}
 
 	public static int hash() {
@@ -20,5 +25,9 @@ public class HashTest {
 		System.out.println(temp ^ (temp >>> 32));
 
 		return result;
+	}
+	
+	public static boolean areEqual(HashClass h1, HashClass h2) {
+		return h1.hashCode() == h2.hashCode();
 	}
 }
