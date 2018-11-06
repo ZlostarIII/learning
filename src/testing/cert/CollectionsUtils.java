8294ca1176f5063e9f931f748eb1f431f41921f2
vsaev <vlado_l_s@yahoo.it>
2018-11-06 23:56:06 +0200

    Streams and Collections changed

--- src/testing/cert/CollectionsUtils.java
+++ src/testing/cert/CollectionsUtils.java
@@ -1,52 +1,64 @@
 package testing.cert;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class CollectionsUtils {
-	public static void main(String[] args) {
-		System.out.println("Deque:");
-		Deque<String> deq = new LinkedList<>(); // Queue<String> q = new
-												// ArrayDeque<>();
-		deq.add("a1");
-		deq.add("a2");
-		deq.add("a3");
-		deq.add("a4");
-		deq.add("a5");
+    public static void main(String[] args) {
+        System.out.println("Deque:");
+        Deque<String> deq = new LinkedList<>(); // Queue<String> q = new
+        // ArrayDeque<>();
+        deq.add("a1");
+        deq.add("a2");
+        deq.add("a3");
+        deq.add("a4");
+        deq.add("a5");
 
-		System.out.println("poll: " + deq.poll());
-		System.out.println("peek: " + deq.peek());
-		System.out.println("poll: " + deq.poll());
-		System.out.println("pop: " + deq.pop());
+        System.out.println("poll: " + deq.poll());
+        System.out.println("peek: " + deq.peek());
+        System.out.println("poll: " + deq.poll());
+        System.out.println("pop: " + deq.pop());
 
-		deq.forEach(System.out::println);
+        deq.forEach(System.out::println);
 
-		Queue<String> q = new ArrayDeque<>();
-		q.add("a1");
-		q.add("a2");
-		q.add("a3");
-		q.add("a4");
-		q.add("a5");
+        Queue<String> q = new ArrayDeque<>();
+        q.add("a1");
+        q.add("a2");
+        q.add("a3");
+        q.add("a4");
+        q.add("a5");
 
-		System.out.println();
+        System.out.println();
 
-		System.out.println("Queue:");
-		q.stream().filter(s -> Integer.valueOf(s.substring(1)) % 2 == 0).forEach(System.out::println);
+        System.out.println("Queue:");
+        q.stream().filter(s -> Integer.valueOf(s.substring(1)) % 2 == 0).forEach(System.out::println);
 
-		System.out.println("poll: " + q.poll());
-		System.out.println("peek: " + q.peek());
-		System.out.println("poll: " + q.poll());
-		System.out.println("pop: " + ((Deque<String>) q).pop());
+        System.out.println("poll: " + q.poll());
+        System.out.println("peek: " + q.peek());
+        System.out.println("poll: " + q.poll());
+        System.out.println("pop: " + ((Deque<String>) q).pop());
 
-		q.stream().filter(s -> Integer.valueOf(s.substring(1)) % 2 == 0).forEach(System.out::println);
+        q.stream().filter(s -> Integer.valueOf(s.substring(1)) % 2 == 0).forEach(System.out::println);
 
-		Map<Integer, String> m = new HashMap<>(2) {{
-			put(1, "s1");
-			put(2, "s2");
-			put(3, "s3");
-			put(4, "s4");
-		}};
+        Map<Integer, String> m = new HashMap<>(2) {{
+            put(1, "s1");
+            put(2, "s2");
+            put(3, "s3");
+        }};
 
-		System.out.println("Hashes: " + m.get(1).hashCode() + ", " + m.get(3).hashCode());
-		System.out.println("Initial capacity is resized to: " + m.entrySet().size());
-	}
+        int i = 1;
+        for (Map.Entry<Integer, String> entry : m.entrySet()) {
+            System.out.println("Hash of element " + entry.getValue() + ", " + i + " is " + entry.getValue().hashCode());
+            i++;
+        }
+
+        m.forEach((key, value) -> {
+            System.out.println("Hash of element " + value + " is " + value.hashCode());
+        });
+
+        m.entrySet().stream()
+                .filter(e -> e.getValue().startsWith("s"))
+                .peek(e -> System.out.println("Lambda: " + e.getValue()))
+                .collect(Collectors.toList());
+    }
 }
