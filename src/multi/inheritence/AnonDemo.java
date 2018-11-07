36fd9f728f1893120a744de7f463c37e7c074936
vsaev <vlado_l_s@yahoo.it>
2018-11-07 22:22:02 +0200

    Lots of changes

--- src/multi/inheritence/AnonDemo.java
+++ src/multi/inheritence/AnonDemo.java
@@ -6,43 +6,43 @@ class Anon {
 }
 
 public class AnonDemo {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		new Anon() {
-			private void hello() {
-				System.out.println("Woah!!! ");
-			}
-		}.hello();
+        new Anon() {
+            private void hello() {
+                System.out.println("Woah!!! ");
+            }
+        }.hello();
 
-		Anon anonInner = new Anon() {
+        Anon anonInner = new Anon() {
 
-			@Override
-			public String toString() {
-				return "Overriden";
-			}
+            @Override
+            public String toString() {
+                return "Overriden";
+            }
 
-			@SuppressWarnings("unused")
-			private void hello() {
-				System.out.println("Hello");
-			}
-		};
-		System.out.println(anonInner.toString());
-		// anonInner.hello(); // Won't compile!
+            @SuppressWarnings("unused")
+            private void hello() {
+                System.out.println("Hello");
+            }
+        };
+        System.out.println(anonInner.toString());
+        // anonInner.hello(); // Won't compile!
 
-		Method meth = anonInner.getClass().getDeclaredMethod("hello");
-		meth.setAccessible(true);
-		meth.invoke(anonInner);
-		// anonInner.getClass().getMethod("hello").invoke(anonInner); // Won't compile!
+        Method meth = anonInner.getClass().getDeclaredMethod("hello");
+        meth.setAccessible(true);
+        meth.invoke(anonInner);
+        // anonInner.getClass().getMethod("hello").invoke(anonInner); // Won't compile!
 
-		// @formatter:off
-		var anonInner2 = new Anon() {
-	          private void hello() {
-	                   System.out.println("New method here, and you can easily access me in Java 10+!\n" +
-	                  "The class is:  " + this.getClass());
-	          }
-		};
-		
-		anonInner2.hello(); // Works!!
-		// @formatter:on
-	}
+        // @formatter:off
+        var anonInner2 = new Anon() {
+            private void hello() {
+                System.out.println("New method here, and you can easily access me in Java 10+!\n" +
+                        "The class is:  " + this.getClass());
+            }
+        };
+
+        anonInner2.hello(); // Works!!
+        // @formatter:on
+    }
 }
--- src/testing/oop/Abstraction.java
+++ src/testing/oop/Abstraction.java
@@ -9,9 +9,11 @@ public abstract class Abstraction {
 	public Abstraction(String s) {
 		this.s = s;
 	}
-	
+
 	public abstract void testAbstraction();
-	
+//	public abstract void testAbstraction2();
+//	public abstract void testAbstraction3();
+
 	public void setString() {
 		s = "Abstraction...";
 		
