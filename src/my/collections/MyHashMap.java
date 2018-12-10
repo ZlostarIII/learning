cb891b54b5e120a0087cd1beface0a78faa00c43
vsaev <vlado_l_s@yahoo.it>
2018-12-10 17:14:48 +0200

    My hashmap and tree changes

--- src/my/collections/MyHashMap.java
+++ src/my/collections/MyHashMap.java
@@ -10,127 +10,184 @@ import java.util.stream.Stream;
 
 public class MyHashMap<K, V> implements Map<K, V> {
 
-	private int size;
-	private final int INITIAL_CAPACITY = 16;
-	private Node<K, V>[] initMap;
+    private int size;
+    private final int INITIAL_CAPACITY = 16;
+    private Node<K, V>[] entries;
 
-	public MyHashMap() {
-		initMap = new Node[INITIAL_CAPACITY];
-	}
+    public MyHashMap() {
+        entries = new Node[INITIAL_CAPACITY];
+    }
 
-	private static class Node<K, V> implements Map.Entry<K, V> {
-		K key;
-		V value;
-		Node<K, V> next;
+    public MyHashMap(int capacity) {
+        entries = new Node[capacity];
+    }
 
-		public K getKey() {
-			return key;
-		}
+    private static class Node<K, V> implements Map.Entry<K, V> {
+        K key;
+        V value;
+        Node<K, V> next;
 
-		public V getValue() {
-			return value;
-		}
+        public Node(K key, V value, Node<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
 
-		public V setValue(V value) {
-			this.value = value;
-			return value;
-		}
+        public K getKey() {
+            return key;
+        }
 
-		public Node<K, V> getNext() {
-			return next;
-		}
+        public V getValue() {
+            return value;
+        }
 
-		public void setNext(Node<K, V> next) {
-			this.next = next;
-		}
+        public V setValue(V value) {
+            this.value = value;
+            return value;
+        }
 
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (o == null || getClass() != o.getClass()) return false;
-			Node<?, ?> node = (Node<?, ?>) o;
-			return Objects.equals(key, node.key) &&
-					Objects.equals(value, node.value) &&
-					Objects.equals(next, node.next);
-		}
+        public Node<K, V> getNext() {
+            return next;
+        }
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(key, value, next);
-		}
-	}
+        public void setNext(Node<K, V> next) {
+            this.next = next;
+        }
 
-	@Override
-	public int size() {
-		return this.size;
-	}
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node<?, ?> node = (Node<?, ?>) o;
+            return Objects.equals(key, node.key) &&
+                    Objects.equals(value, node.value) &&
+                    Objects.equals(next, node.next);
+        }
 
-	@Override
-	public boolean isEmpty() {
-		return this.size == 0;
-	}
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value, next);
+        }
+    }
 
-	@Override
-	public boolean containsKey(Object key) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public int size() {
+        return this.size;
+    }
 
-	@Override
-	public boolean containsValue(Object value) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 
-	@Override
-	public V get(Object key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean containsKey(Object key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public V put(K key, V value) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean containsValue(Object value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public V remove(Object key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public V get(Object key) {
+        int hash = hash(key);
 
-	@Override
-	public void putAll(Map<? extends K, ? extends V> m) {
-		// TODO Auto-generated method stub
-		
-	}
+        if (entries[hash] == null) {
+            return null;
+        } else {
+            Node<K, V> current = entries[hash];
 
-	@Override
-	public void clear() {
-		initMap = null;
-	}
+            while (current != null) {
+                if (current.key.equals(key)) {
+                    return current.value;
+                }
+                current = current.next;
+            }
 
-	@Override
-	public Set<K> keySet() {
-		Set<K> keys = new HashSet<>();
-		for(Node<K, V> n : initMap) {
-			keys.add(n.getKey());
-		}
-		return keys;
-	}
+            return null;
+        }
+    }
 
-	@Override
-	public Collection<V> values() {
+    @Override
+    public V put(K key, V value) {
+        Node<K, V> entry = new Node<>(key, value, null);
+
+        int hash = hash(key);
+
+        Node<K, V> current = entries[hash];
+
+        if (current == null) {
+            entries[hash] = entry;
+            size++;
+        } else {
+            while (current != null) {
+                if (current.key.equals(key)) {
+                    current.value = value;
+                } else {
+                    current.next = entry;
+                    size++;
+                }
+                current = current.next;
+            }
+        }
+
+        return entries[hash].getValue();
+    }
+
+    @Override
+    public V remove(Object key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void clear() {
+        entries = null;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> keys = new HashSet<>();
+        for (Node<K, V> n : entries) {
+            keys.add(n.getKey());
+        }
+        return keys;
+    }
+
+    @Override
+    public Collection<V> values() {
 //		Collection<K> values = Stream.of(initMap).forEach(v -> v.getValue()).collect(Collectors.toCollection());
-		Collection<K> values = null;
-		return (Collection<V>) values;
-	}
+        Collection<K> values = null;
+        return (Collection<V>) values;
+    }
 
-	@Override
-	public Set<Entry<K, V>> entrySet() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
+    private int hash(Object key) {
+        return Math.abs(key.hashCode()) % entries.length;
+    }
+
+    public static void main(String[] args) {
+        MyHashMap<Integer, String> myMap = new MyHashMap<>();
+        myMap.put(1, "first");
+        myMap.put(2, "second");
+        myMap.put(3, "third");
+        myMap.put(2, "forth");
+
+        System.out.println(myMap.get(2));
+    }
 }
--- src/my/collections/MyTree.java
+++ src/my/collections/MyTree.java
@@ -2,131 +2,124 @@ package my.collections;
 
 public class MyTree {
 
-	private Node root;
+    private Node root;
 
-	public MyTree() {
-		this.root = null;
-	}
+    public MyTree() {
+        this.root = null;
+    }
 
-	public void addChild(Object data, Node node) {
-		node = new Node(data);
-	}
+    public void preOrder() {
+        System.out.print("Pre-order traversal: ");
+        preOrder(root);
+        System.out.println("\n==================");
+    }
 
-	public void preOrder() {
-		System.out.print("Pre-order traversal: ");
-		preOrder(root);
-		System.out.println("\n==================");
-	}
+    private void preOrder(Node node) {
+        if (node == null)
+            return;
 
-	private void preOrder(Node node) {
-		if (node == null)
-			return;
-		
-		System.out.print(node.data + " ");
-		preOrder(node.left);
-		preOrder(node.right);
-	}
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
 
-	public void postOrder() {
-		System.out.print("Post-order traversal: ");
-		postOrder(root);
-		System.out.println("\n==================");
-	}
+    public void postOrder() {
+        System.out.print("Post-order traversal: ");
+        postOrder(root);
+        System.out.println("\n==================");
+    }
 
-	private void postOrder(Node node) {
-		if (node == null)
-			return;
-		
-		postOrder(node.left);
-		postOrder(node.right);
-		System.out.print(node.data + " ");
-	}
+    private void postOrder(Node node) {
+        if (node == null)
+            return;
 
-	public void inOrder() {
-		System.out.print("In-order traversal: ");
-		inOrder(root);
-		System.out.println("\n==================");
-	}
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + " ");
+    }
 
-	private void inOrder(Node node) {
-		if (node == null)
-			return;
-		
-		inOrder(node.left);
-		System.out.print(node.data + " ");
-		inOrder(node.right);
-	}
+    public void inOrder() {
+        System.out.print("In-order traversal: ");
+        inOrder(root);
+        System.out.println("\n==================");
+    }
 
-	private static class Node {
-		Object data;
-		Node left, right;
+    private void inOrder(Node node) {
+        if (node == null)
+            return;
 
-		public Node(Object data) {
-			this.data = data;
-		}
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((data == null) ? 0 : data.hashCode());
-			result = prime * result + ((left == null) ? 0 : left.hashCode());
-			result = prime * result + ((right == null) ? 0 : right.hashCode());
-			return result;
-		}
+    private static class Node {
+        Object data;
+        Node left, right;
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Node other = (Node) obj;
-			if (data == null) {
-				if (other.data != null)
-					return false;
-			} else if (!data.equals(other.data))
-				return false;
-			if (left == null) {
-				if (other.left != null)
-					return false;
-			} else if (!left.equals(other.left))
-				return false;
-			if (right == null) {
-				if (other.right != null)
-					return false;
-			} else if (!right.equals(other.right))
-				return false;
-			return true;
-		}
+        public Node(Object data) {
+            this.data = data;
+        }
 
-	}
-	
-	public static void main(String[] args) {
-		MyTree myTree = new MyTree();
-		myTree.root = new Node("Root");
-		myTree.root.left = new Node("Child1");
-		myTree.root.right = new Node("Child2");
-		myTree.root.left.left = new Node("Child3");
-		myTree.root.right.left = new Node("Child4");
-		myTree.root.right.right = new Node("Child5");
-		myTree.root.right.right.right = new Node("Child6");
-		myTree.root.left.left.right = new Node("Child7");
-		myTree.root.left.left.right.left = new Node("Child8");
-		
-//		myTree.addChild("Child1", myTree.root.left);
-//		myTree.addChild("Child2", myTree.root.right);
-//		myTree.addChild("Child3", myTree.root.left.left);
-//		myTree.addChild("Child4", myTree.root.right.left);
-//		myTree.addChild("Child5", myTree.root.right.right);
-//		myTree.addChild("Child6", myTree.root.right.right.right);
-//		myTree.addChild("Child7", myTree.root.left.left.right);
-//		myTree.addChild("Child8", myTree.root.left.left.right.left);
-		
-		myTree.preOrder();
-		myTree.postOrder();
-		myTree.inOrder();
-	}
+        public Node(Object data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((data == null) ? 0 : data.hashCode());
+            result = prime * result + ((left == null) ? 0 : left.hashCode());
+            result = prime * result + ((right == null) ? 0 : right.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Node other = (Node) obj;
+            if (data == null) {
+                if (other.data != null)
+                    return false;
+            } else if (!data.equals(other.data))
+                return false;
+            if (left == null) {
+                if (other.left != null)
+                    return false;
+            } else if (!left.equals(other.left))
+                return false;
+            if (right == null) {
+                if (other.right != null)
+                    return false;
+            } else if (!right.equals(other.right))
+                return false;
+            return true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MyTree myTree = new MyTree();
+        myTree.root = new Node("Root");
+        myTree.root.left = new Node("Child1");
+        myTree.root.right = new Node("Child2");
+        myTree.root.left.left = new Node("Child3");
+        myTree.root.right.left = new Node("Child4");
+        myTree.root.right.right = new Node("Child5");
+        myTree.root.right.right.right = new Node("Child6");
+        myTree.root.left.left.right = new Node("Child7");
+        myTree.root.left.left.right.left = new Node("Child8");
+
+        myTree.preOrder();
+        myTree.postOrder();
+        myTree.inOrder();
+    }
 }
